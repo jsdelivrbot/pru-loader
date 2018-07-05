3f9d1f9e95ce11a3c300cc435f45869e60aa7eff
sam <your.email@here.com>
2018-07-04 22:29:46 -0700

    simple shared buffer works

--- shelly.sh
+++ shelly.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 # This line is a comment.
 
-echo 'BR2_PACKAGE_PRU_USER=y' >> .config
-echo 'BR2_PACKAGE_PRU_CTRL=y' >> .config
+echo 'BR2_PACKAGE_PRU_USER=y'      >> .config
+echo 'BR2_PACKAGE_PRU_CTRL=y'      >> .config
+echo 'BR2_PACKAGE_SHBUF=y'         >> .config
+echo 'BR2_PACKAGE_SHBUF_USER=y'    >> .config
 
 rm -r output/build/pru_ctrl-1.0/
 rm -r output/build/pru_user-1.0/
+rm -r output/build/shbuf-1.0/
+rm -r output/build/shbuf_user-1.0/
 
 make -j12
 
